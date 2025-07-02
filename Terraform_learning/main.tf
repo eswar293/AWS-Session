5801c93448372e4d8ccd1a784b1e6415d73dcea0
Eswar Kumar <eswarpkumar@gmail.com>
2025-07-02 11:38:17 +0530

    updated terraform main.tf file

---
+++ Terraform_learning/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "learn-terraform"
+  }
+}
+
