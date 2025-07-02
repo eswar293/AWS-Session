766c08c0c6998784e794f9d879ac1096ab92a844
Eswar Kumar <eswarpkumar@gmail.com>
2025-07-02 11:45:47 +0530

    file terraform.tf

---
+++ Terraform_learning/terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket         = "your-terraform-state-bucket"
+    key            = "path/to/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "your-lock-table"
+    encrypt        = true
+  }
+  required_version = ">= 1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
